d93ac8b06221900c9333b18cd7a8c3536ca2c959
SoWiEee <kawiisowieee@gmail.com>
2025-11-26 00:32:55 +0800

    Update comment

--- engine/rendering/Mesh.cpp
+++ engine/rendering/Mesh.cpp
@@ -16,43 +16,34 @@ Mesh::~Mesh() {
 }
 
 void Mesh::SetupMesh() {
-    // 1. 產生 Buffer ID
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
     if (!m_Indices.empty()) {
         glGenBuffers(1, &EBO);
     }
 
-    // 2. 綁定 VAO (開始記錄狀態)
     glBindVertexArray(VAO);
 
-    // 3. 設定 VBO (頂點資料)
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    // 使用 sizeof(Vertex) 可以直接取得結構體的大小
     glBufferData(GL_ARRAY_BUFFER, m_Vertices.size() * sizeof(Vertex), &m_Vertices[0], GL_STATIC_DRAW);
 
-    // 4. 設定 EBO (索引資料 - 如果有的話)
     if (!m_Indices.empty()) {
         glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
         glBufferData(GL_ELEMENT_ARRAY_BUFFER, m_Indices.size() * sizeof(unsigned int), &m_Indices[0], GL_STATIC_DRAW);
     }
 
-    // 5. 設定頂點屬性指標 (Vertex Attribute Pointers)
-    // 這些 offsetof 是根據 struct Vertex 的記憶體佈局自動計算的
-
-    // layout(location = 0) in vec3 aPos;
+    // Vertex Positions
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)0);
 
-    // layout(location = 1) in vec2 aTexCoord;
+    // Vertex Texture Coords
     glEnableVertexAttribArray(1);
     glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, TexCoords));
 
-    // layout(location = 2) in vec3 aNormal;
+    // Vertex Normals
     glEnableVertexAttribArray(2);
     glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, Normal));
 
-    // 6. 解除綁定 VAO (避免不小心修改到)
     glBindVertexArray(0);
 }
 
--- engine/rendering/Mesh.h
+++ engine/rendering/Mesh.h
@@ -18,17 +18,11 @@ public:
     void Bind() const;
     void Unbind() const;
 
-    // 取得頂點數量 (如果沒有索引，就回傳頂點數；有索引回傳索引數)
-    // 這對於 glDrawArrays 和 glDrawElements 都很有用
     int GetCount() const { return m_Indices.empty() ? (int)m_Vertices.size() : (int)m_Indices.size(); }
-
-    // 檢查是否有 EBO (索引緩衝)
     bool HasIndices() const { return !m_Indices.empty(); }
 
 private:
     unsigned int VAO, VBO, EBO;
-
-    // 儲存資料副本 (方便之後如果要做碰撞檢測或修改模型)
     std::vector<Vertex> m_Vertices;
     std::vector<unsigned int> m_Indices;
 
