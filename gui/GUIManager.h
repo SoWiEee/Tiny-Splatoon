3bd820268a8a4b2a66f5867f167a8c65baee2f63
SoWiEee <kawiisowieee@gmail.com>
2025-11-27 21:24:28 +0800

    Fix include file

--- gui/GUIManager.h
+++ gui/GUIManager.h
@@ -1,10 +1,11 @@
 #pragma once
 #include <imgui.h>
-#include <backends/imgui_impl_glfw.h>
-#include <backends/imgui_impl_opengl3.h>
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
 #include <string>
 #include <vector>
 #include <glm/glm.hpp>
+#include <GLFW/glfw3.h>
 #include "../network/NetworkManager.h"
 #include "../network/NetworkProtocol.h"
 
